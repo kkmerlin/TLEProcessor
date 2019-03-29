672b82dc9cbf78353c42c2275059bf7133e9a5c7
Kishan Kumar <37082230+kkmerlin@users.noreply.github.com>
2019-03-29 12:32:43 +0530

    Update DOPPLERSHIFT.py

--- DOPPLERSHIFT.py
+++ DOPPLERSHIFT.py
@@ -62,18 +62,18 @@ st3 = np.array([ -2940.3, 2473.7, 6555.8])#Station 3
 
 
 #Position vector calculation
-def position(R1,R2,R3,rho1,rho2,rho3):
-    d1,d2=[(R2-R3),(R1-R3)]
+def position(X1,X2,X3,rho1,rho2,rho3):
+    d1,d2=[(X2-X3),(X1-X3)]
     cross=np.cross(d1,d2)
     e=cross/np.linalg.norm(cross)#Unit vector perpendicular to the plane of R1, R2 and R3
     #Rest of the algorithm described in the attached file 
-    f1=np.linalg.norm(R1)**2-rho1**2
-    f2=np.linalg.norm(R2)**2-rho2**2
-    f3=np.linalg.norm(R3)**2-rho3**2
+    f1=np.linalg.norm(X1)**2-rho1**2
+    f2=np.linalg.norm(X2)**2-rho2**2
+    f3=np.linalg.norm(X3)**2-rho3**2
     f=(1/np.linalg.norm(cross))*((f1-f3)*d1+(f2-f3)*d2)
     F=np.cross(e,f)
-    H=R3.dot(e)
-    G=R3.dot(F)-f3
+    H=X3.dot(e)
+    G=X3.dot(F)-f3
     fsquare=F.dot(F)
     J=H + math.sqrt(H**2 + 2*G + fsquare)#Here -fsquare instead of +fsquare should be taken. But due to inconsistency of the            #randomly      taken data. If actual data from a satellite revolving in elliptical orbit is taken, the inconsistency won't occur. 
     r=F+J*e
@@ -81,10 +81,91 @@ def position(R1,R2,R3,rho1,rho2,rho3):
 
 
 #three pposition vectors for calculation of orbital parameters    
-print(position(st1,st2,st3,p1,p2,p3))
-print(position(st1,st2,st3,q1,q2,q3))
-print(position(st1,st2,st3,r1,r2,r3))
-#now Orbital parameters can be calculated using Gibb's method     
+R1=(position(st1,st2,st3,p1,p2,p3))
+R2=(position(st1,st2,st3,q1,q2,q3))
+R3=(position(st1,st2,st3,r1,r2,r3))
+#now Orbital parameters can be calculated using Gibb's method  
+#Constants
+mu = 398600.4418;
+pi = 3.141592653
+#Implementation of Gibb's method   
+r1 = np.linalg.norm(R1)
+r2 = np.linalg.norm(R2)
+r3 = np.linalg.norm(R3)
+
+c12 = np.cross(R1,R2)
+c23 = np.cross(R2,R3)
+c31 = np.cross(R3,R1)
+
+##Calculate D and N vectors
+##Orthogonal to the plane traced by the position vectors
+N = r1*c23 + r2*c31 + r3*c12
+D = c12 + c23 + c31
+
+## Calcualte P, from pD = N
+
+p = np.linalg.norm(N)/np.linalg.norm(D)
+
+## Calculate S, then find e using e = S/D
+
+S = R1*(r2 - r3) + R2*(r3 - r1) + R3*(r1 - r2)
+e = np.linalg.norm(S)/np.linalg.norm(D)
+
+## Q = w x r, calculate w then find Q
+
+W = N/np.linalg.norm(N)
+Q = S/np.linalg.norm(S)
+
+## Calculate B and L
+
+B = np.cross(D,R2);
+L = math.sqrt(mu/(np.linalg.norm(D) * np.linalg.norm(N)));
+
+## Calculate V2
+v2 = (np.linalg.norm(L)/r2)*B + np.linalg.norm(L) * S;
+
+## Using R2 and v2, calculate orbital elements
+
+H = np.cross(R2,v2);
+
+##Calculate i (inclination)
+inclination = math.acos(H[2]/np.linalg.norm(H));
+inclination = inclination * 180/pi;
+
+n = [-H[1], H[0], 0];
+
+v=np.linalg.norm(v2);
+r=np.linalg.norm(R2);
+
+vr = np.dot(R2,v2)/r;
+
+E = 1/mu*((v*v - mu/r)*R2 - np.dot(R2,v2)*v2);
+
+##Calculate Omega (longitude of the ascending node)
+
+Omega = math.acos(n[0]/np.linalg.norm(n)) * 180/pi;
+
+##Calculate omega (argument of periapsis)
+
+omega = math.acos(np.dot(n,E)/(np.linalg.norm(n) * np.linalg.norm(E))) * 180/pi;
+
+##Calculate nu (mean anomaly)
+
+nu = math.acos(np.dot(E,R2)/(np.linalg.norm(E) * np.linalg.norm(R2))) * 180/pi;
+
+##Calculate eccentricity
+e = np.linalg.norm(E);
+
+## Calculate orbital major axis
+
+a = p / (1-(e*e));
+
+print("Eccentricity:",e)
+print("Inclination:",inclination)
+print("Argument of Perigee:",omega)
+print("Mean Anomaly:",nu)
+print("Semi major axis:",a)
+print("Right ascension of ascending node:",Omega)
 
 
 
